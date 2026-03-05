b9f2d25c3d09e38cf23bc594da404455cf7ef141
chimpin <cbonn279@gmail.com>
2026-03-04 20:32:27 -0800

    first commit

---
+++ src/scenes/main.js
@@ -0,0 +1,22 @@
+// Christian Bonardi
+// Cube World
+// last updated: 3/4/2026
+
+const config = {
+    parent: 'phaser-game',
+    type: Phaser.WEBGL,
+    width: 1024,
+    height: 600,
+    backgroundColor: '#000000',   
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 2000 },
+            debug: false
+        }
+    },
+    scene: [ Load, Menu, Play, GameOver ]
+}
+
+const game = new Phaser.Game(config)
